251670c2679042484a8ad631f51c1c5316aba83e
Thomas Sigdestad <tsi@enonic.com>
2019-08-29 13:33:54 +0200

    fixed error handler example

--- docs/framework/code/error.js
+++ docs/framework/code/error.js
@@ -1,29 +1,25 @@
-var thymeleaf = require('/lib/xp/thymeleaf');
-
-var view404 = resolve('page-not-found.html');
-var viewGeneric = resolve('error.html');
-
 exports.handle404 = function (err) {
-    var body = thymeleaf.render(view404, {});
-    return {
-        contentType: 'text/html',
-        body: body
-    }
+  return {
+    contentType: 'text/html',
+    body: `
+<html>
+  <body>
+      <h1>No page for you!</h1>
+  </body>
+</html>
+`
+  }
 };
 
 exports.handleError = function (err) {
-    var debugMode = err.request.params.debug === 'true';
-    if (debugMode && err.request.mode === 'preview') {
-        return;
-    }
-
-    var params = {
-        errorCode: err.status
-    };
-    var body = thymeleaf.render(viewGeneric, params);
-
-    return {
-        contentType: 'text/html',
-        body: body
-    }
+  return {
+    contentType: 'text/html',
+    body: `
+<html>
+  <body>
+      <h1>Error code "${err.status}" </h1>
+  </body>
+</html>
+`
+  }
 };
