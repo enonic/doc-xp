b9b442f0033b3d817f979647aad9c407f16ba1e4
sigdestad <sigdestad@users.noreply.github.com>
2023-12-12 19:27:58 +0000

    deploy: 41a28622f32bc0f1e4de9322b64fd9026deb6739

--- stable/framework/code/main-2.js
+++
@@ -1,7 +0,0 @@
-// Log application started
-log.info('Application ' + app.name + ' started');
-
-// Log when application is stopped
-__.disposer(function() {
-    log.info('Application ' + app.name + ' stopped');
-});
