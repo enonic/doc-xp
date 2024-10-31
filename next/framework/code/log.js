0da4ea79b8b531ee973c62781eb406eac754fa46
alansemenov <alansemenov@users.noreply.github.com>
2024-10-31 09:49:49 +0000

    deploy: b100e36f3896206cdb81dea1f0df9a1834f7570e

---
+++ next/framework/code/log.js
@@ -0,0 +1,11 @@
+// Log a simple message
+log.debug('Hello World');
+
+// Log a formatting message
+log.info('Hello %s', 'World');
+
+// Log a formatting message
+log.warning('%s %s', 'Hello', 'World');
+
+// Log using the built-in JSON converter
+log.error('My JSON %s', object );
