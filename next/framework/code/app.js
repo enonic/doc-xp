08519b4a00ac3aedde6253b7d5f382025e8586c2
alansemenov <alansemenov@users.noreply.github.com>
2024-11-01 10:11:15 +0000

    deploy: 2379c0ecdb00e3bc8e5ac44bd15b8c9132b45637

--- next/framework/code/app.js
+++
@@ -1,8 +0,0 @@
-// Get application name
-var name = app.name;  // com.enonic.app.superhero
-
-// Get application version
-var version = app.version;  // 1.2.0
-
-// Get some config from the <app.name>.cfg file
-var myKey = app.config.secretkey; // Reads the string stored in the "secretkey" property
