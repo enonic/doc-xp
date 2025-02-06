4d2ccfb6b4ab3714fac19c97f0a67730700b6fb0
rymsha <rymsha@users.noreply.github.com>
2025-02-06 15:16:10 +0000

    deploy: d4033c9b5b0f3210ff023867bc95d7c093f44cfd

---
+++ next/framework/code/require.js
@@ -0,0 +1,12 @@
+
+// Absolute path
+var lib1 = require('/lib/mylib.js');
+
+// Relative path
+var lib2 = require('mylib');
+
+// Relative path (same as above)
+var lib3 = require('./mylib.js');
+
+// Relative path
+var lib4 = require('../mylib');
