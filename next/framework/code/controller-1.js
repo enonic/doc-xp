f3211ed68cc979a2bf3a68b93796d9dd0a6559b8
sigdestad <sigdestad@users.noreply.github.com>
2026-03-17 10:14:53 +0000

    deploy: 70b8738ea573d2c9f31d67a19be4c713ee4d0959

--- next/framework/code/controller-1.js
+++
@@ -1,8 +0,0 @@
-// Handles a GET request
-exports.GET = function(req) {}
-
-// Handles a POST request
-exports.POST = function(req) {}
-
-// Handles all requests, other than GET or POST which are handled by the functions above
-exports.all = function(req) {}
