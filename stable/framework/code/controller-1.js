c6b2686cfcf2b84f3829765393239931a256fe58
sigdestad <sigdestad@users.noreply.github.com>
2026-06-03 13:20:30 +0000

    deploy: be170141ac6ebc44075e9985ab4e57b2df9f437b

--- stable/framework/code/controller-1.js
+++
@@ -1,8 +0,0 @@
-// Handles a GET request
-exports.get = function(req) {}
-
-// Handles a POST request
-exports.post = function(req) {}
-
-// Handles all requests, other than GET or POST which are handled by the functions above
-exports.all = function(req) {}
